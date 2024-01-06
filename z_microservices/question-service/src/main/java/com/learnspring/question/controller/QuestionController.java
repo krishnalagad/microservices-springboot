7b3db38586f355f42ce826abaa212eb83792c42d
Krishna Lagad <krishnalagad2@gmail.com>
2024-01-06 13:04:21 +0530

    Update QuestionController.java

--- z_microservices/question-service/src/main/java/com/learnspring/question/controller/QuestionController.java
+++ z_microservices/question-service/src/main/java/com/learnspring/question/controller/QuestionController.java
@@ -10,7 +10,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/question")
 public class QuestionController {
-
     private final QuestionService questionService;
 
     public QuestionController(QuestionService questionService) {
