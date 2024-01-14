c289eb1559fae73740e3e434d94cb08abbe3bd61
Krishna Lagad <krishnalagad2@gmail.com>
2024-01-14 11:08:59 +0530

    Update QuestionRepository.java

--- z_microservices/question-service/src/main/java/com/learnspring/question/repository/QuestionRepository.java
+++ z_microservices/question-service/src/main/java/com/learnspring/question/repository/QuestionRepository.java
@@ -3,5 +3,9 @@ package com.learnspring.question.repository;
 import com.learnspring.question.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    List<Question> getByQuizId(Long id);
 }
